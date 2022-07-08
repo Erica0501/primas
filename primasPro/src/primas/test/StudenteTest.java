83bd794032576b37d1853a02ef785daf689d05c0
Marco Piccioni <marcotest65@gmail.com>
2022-07-08 16:32:47 +0200

    Update StudenteTest.java
    Attributes are private

--- primasPro/src/primas/test/StudenteTest.java
+++ primasPro/src/primas/test/StudenteTest.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class StudenteTest {
 
-	List<Studente> stu;
+	private List<Studente> stu;
 
 	@Before                                        
 	public void setUp() {
