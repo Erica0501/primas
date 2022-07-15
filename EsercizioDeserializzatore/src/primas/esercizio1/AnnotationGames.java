9e651aa0df164e15e5dd35cccb1502fc7c65568e
Arcadie <boisteanu19@gmail.com>
2022-07-15 12:23:23 +0200

    Aggiunto il file auto.json sotto resources/json/,imposta letture json nel main.

--- EsercizioDeserializzatore/src/primas/esercizio1/AnnotationGames.java
+++ EsercizioDeserializzatore/src/primas/esercizio1/AnnotationGames.java
@@ -1,64 +1,42 @@
 package primas.esercizio1;
 
-import java.io.IOException;
-import java.text.ParseException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
 
 public class AnnotationGames {
 
-	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, IOException, ParseException{
+    public static void main(String[] args) throws Exception {
 
-		Automobile auto = new Automobile("Toyota", "Corolla", 2001);
-		System.out.println("Auto da serializzare: " + auto);
-		JSONSimpleSerializer my_serializer = new JSONSimpleSerializer();
+        Automobile auto = new Automobile("Toyota", "Corolla", 2001);
+        Assicurazione assicur = new Assicurazione(LocalDate.parse("2020-02-17"), LocalDate.parse("2023-07-09"));
+        auto.setAssicurazione(assicur);
+        //System.out.println("Auto da serializzare: " + auto);
+        JSONSimpleSerializer my_serializer = new JSONSimpleSerializer();
+        Gson gson = new Gson();
+        List<Object> list = getListObjectFromFileJson(gson);
 
-		String jsonString = 
-				//		"{\"marca\":\"BMW\", \"modello\":\"955e\", \"anno\":\"2025\"}";
+        String jsonString = gson.toJson(list.get(5));
+        Automobile auto3 = my_serializer.deserialize(jsonString, Automobile.class);
+        System.out.println("Auto deserializzata: " + auto3);
 
-				//"{\"marca\":\"BMW\",\"modello\":\"955e\",\"anno\":\"2025\",\"assicurazione\":{\"inizioContratto\":\"2022-07-08\",\"scadenzaContratto\":\"2023-01-08\"}}";
+    }
 
-				//	"{\"marca\":\"BMW\",\"modello\":\"955e\",\"assicurazione\":{\"inizioContratto\":\"2022-07-08\",\"scadenzaContratto\":\"2023-01-08\"},\"anno\":\"2025\"}";
+    public static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
 
-				//"{\"assicurazione\":{\"inizioContratto\":\"2022-07-08\",\"scadenzaContratto\":\"2023-01-08\"},\"marca\":\"BMW\",\"modello\":\"955e\",\"anno\":\"2025\"}";
-
-				//"{\"assicurazione\":{\"inizioContratto\":\"2022-07-08\",\"scadenzaContratto\":\"2023-01-08\"},\"marca\":\"BMW\",\"modello\":\"955e\",\"allestimento\":[\"base\",\"advantage\",\"Msport\",\"goldEdition\"],\"anno\":\"2025\"}";
-
-				"{\"assicurazione\":{\"inizioContratto\":\"2022-07-08\",\"scadenzaContratto\":\"2023-01-08\"},\r\n"
-				+ "\"persona\":{\r\n"
-				+ "    \"nome\":\"Giovanna\",\r\n"
-				+ "    \"cognome\":\"Ben Detto\",\r\n"
-				+ "	\"eta\":42,\r\n"
-				+ "  \"cartaIdentita\":{\r\n"
-				+ "    \"nome\":\"Giovanna\",\r\n"
-				+ "    \"cognome\":\"Ben Detto\",\r\n"
-				+ "    \"comune\":\"Frosinone\",\r\n"
-				+ "    \"nazionalita\":\"Italia\",\r\n"
-				+ "    \"sesso\":\"F\",\r\n"
-				+ "    \"dataNascita\":\"1970-11-13\",\r\n"
-				+ "    \"dataEmissione\":\"1989-06-01\",\r\n"
-				+ "    \"dataScadenza\":\"2005-01-21\",\r\n"
-				+ "    \"residenza\":\"Frosinone\",\r\n"
-				+ "    \"via\":\"Vial del grande 5\",\r\n"
-				+ "    \"professione\":\"Insegnante scuola elementare\",\r\n"
-				+ "    \"statura\":\"media\",\r\n"
-				+ "    \"altezza\":177,\r\n"
-				+ "    \"capelli\":\"a caschetto\",\r\n"
-				+ "    \"segniParticolari\":\"malata di pulito\",\r\n"
-				+ "    \"numeroDiSerie\":\"CA038200DE\"\r\n"
-				+ "  },\r\n"
-				+ "  \"patente\":{\r\n"	
-				+ "    \"nome\":\"Giovanna\",\r\n"
-				+ "    \"cognome\":\"Arco\",\r\n"
-				+ "    \"dataNascita\":\"1970-11-13\",\r\n"
-				+ "    \"dataRilascio\":\"1989-06-01\",\r\n"
-				+ "    \"dataScadenza\":\"1999-06-01\",\r\n"
-				+ "    \"numeroPatente\":\"U1H68G400B\",\r\n"
-				+ "    \"categoria\":\"B96\"\r\n"
-				+ "  }\r\n"
-				+ "},\"marca\":\"BMW\",\"modello\":\"955e\",\"allestimento\":[\"base\",\"advantage\",\"Msport\",\"goldEdition\"],\"anno\":\"2025\"}";		
-
-		Automobile auto3 = my_serializer.deserialize(jsonString, Automobile.class);
-		System.out.println("Auto deserializzata: " + auto3);
-
-	}
+    public static List<Object> getListObjectFromFileJson(Gson gson) throws Exception {
+        gson = new Gson();
+        Path path = Paths.get(AnnotationGames.class.getResource("/primas/resources/json/auto.json").toURI());
+        Type listType = new TypeToken<List<Object>>() {}.getType();
+        return gson.fromJson(readFileAsString(path.toString()), listType);
+    }
 }
 
